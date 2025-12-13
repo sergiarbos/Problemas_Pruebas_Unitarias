0320f2acb203360656ee458370fdf62f9c27969d
Sergi Arbós <154980123+sergiarbos@users.noreply.github.com>
2025-12-13 14:06:20 +0100

    Add ReceiptPrinter interface with necessary methods

--- Problema8/ReceiptPrinter.java
+++ Problema8/ReceiptPrinter.java
@@ -1,1 +1,9 @@
+import java.math.BigDecimal;
 
+public interface ReceiptPrinter {
+    void init();
+    void addProduct(String description, int quantity, BigDecimal price);
+    void addTaxes(BigDecimal taxes);
+    void print(BigDecimal total);
+    String getOutput();
+}
