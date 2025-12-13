07677cb9bf9cecce391ea5fdb625e3853735d3db
Sergi Arbós <154980123+sergiarbos@users.noreply.github.com>
2025-12-13 14:05:43 +0100

    Add ProductDTO class with fields and constructor

--- Problema8/ProductDTO.java
+++ Problema8/ProductDTO.java
@@ -1,1 +1,17 @@
+import java.math.BigDecimal;
 
+public class ProductDTO {
+    private String productID;
+    private String description;
+    private BigDecimal price;
+
+    public ProductDTO(String productID, String description, BigDecimal price) {
+        this.productID = productID;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getProductID() { return productID; }
+    public String getDescription() { return description; }
+    public BigDecimal getPrice() { return price; }
+}
